26530ed861eeb09701a4e6f8e841797506f5e685
Danial Tariq <2460201t@student.gla.ac.uk>
2022-02-14 00:25:49 +0000

    Import new upload/download functions in  __init__.py

--- CSP3_project/backend/ivdatahandler/__init__.py
+++ CSP3_project/backend/ivdatahandler/__init__.py
@@ -6,6 +6,8 @@ from pymongo.database import Database
 from .mongomodel import MongoModel, BaseModel
 from .filereader import read_file
 from .config import setup_mongodb_from_file
-from .database import upload_file, download_one
+from .database import (
+    upload_file, upload_bulk, upload_directory,
+    download_one, download_many)
 
 setup_mongodb_from_file(str(Path(__file__).with_name('config.ini')))
