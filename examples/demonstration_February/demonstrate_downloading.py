92c38f3bb4b4b70907f48a4a16a0fa8d52c27b55
Danial Tariq <2460201t@student.gla.ac.uk>
2022-02-14 05:00:48 +0000

    Add files for demonstration for the Customer Day on February

---
+++ examples/demonstration_February/demonstrate_downloading.py
@@ -0,0 +1,36 @@
+from backend.ivdatahandler import download_one, download_many
+from backend.models import IV, Wafer, Die
+from demonstration_Feb import clear_database, upload_all
+
+
+# Uncomment the following two lines if you need to clean up the database before running this script.
+# clear_database()
+# upload_all()
+
+# 1. Download a single document.
+
+# Download a single IV file which matches the criteria into a file called 'test_download.txt'
+download_one(IV, 'test_download.txt', author='Dima Maneuski')
+
+# If no file name is specified, the id of the document will be used.
+download_one(Wafer, name='iLGAD_3374-15')
+
+# You can format the file name yourself, using python format strings.
+# The following will download a iLGAD die, and the name of the file will be
+# the wafer, the die name, and the anode type of the downloaded die.
+download_one(Die, "{wafer}_{name}_{anode_type}.txt", device_type='iLGAD')
+
+# In the above line, the fields in the format string need to match the fields for Die (defined in backend/models.py)
+
+
+# 2. Downloading multiple documents
+
+# Download multiple documents which match the criteria, into a folder called 'downloaded_dies'
+# Like before, we can format the file name as well.
+download_many(Die, directory='downloaded_dies/', filepath="{wafer}_{name}.txt", anode_type="pixels")
+
+# You can further group downloaded documents using the 'group' parameter.
+# The following line of code will download all IVs performed by the University of Glasgow.
+# It will download them into downloaded_ivs directory.
+# The folder will be further subdivided into subdirectories, corresponding to the wafer, and the author.
+download_many(IV, directory='downloaded_ivs/', filepath='IV_{wafer}_{die}.txt', group=['wafer', 'author'], institution='University of Glasgow')
---
+++ examples/demonstration_February/demonstrate_uploading.py
@@ -0,0 +1,26 @@
+from backend.ivdatahandler import upload_file, upload_bulk, upload_directory
+from demonstration_Feb import clear_database
+
+
+clear_database()
+
+# 1. Upload a single file
+upload_file('ivs/IV_wafer_iLGAD_3374-15_die_0_0.txt')
+upload_file('wafers/Wafer_iLGAD_3374-15.txt')
+
+# If a document with the same if already exists in the database, set 'upsert' to True to replace it.
+# If upsert is False, the following line would crash as we've already uploaded this file above.
+upload_file('wafers/Wafer_iLGAD_3374-15.txt', upsert=True)
+
+# 2. Uploading multiple files at a time
+upload_bulk(
+    ['ivs/IV_wafer_iLGAD_3374-16_die_0_1.txt', 'dies/Die_iLGAD_3374-16_0_1.txt', 'wafers/Wafer_iLGAD_3374-16.txt'])
+
+# 3. Uploading a directory
+
+# Upload the entire wafers directory at once.
+upload_directory('wafers/', upsert=True)
+
+# Can also use patterns to upload only specific files in a directory.
+# For example, only upload dies between 0_0 and 0_3
+upload_directory('dies/', pattern='*0_[0-3]*.txt')
