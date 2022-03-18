6bfed43077ca336ac054e96c2422ee8ac2158717
Ciara I S Lösel <2438870l@student.gla.ac.uk>
2022-03-18 13:11:09 +0000

    adds last demo

--- examples/demonstrations/demonstrate_graphing.py
+++ examples/demonstrations/demonstrate_graphing.py
@@ -8,28 +8,45 @@ upload_all()
 
 # 1. Plotting all IV experiments from the same wafer onto the same graph
 
-ivs = IV.find(query={'wafer':'wafer_name'})
+ivs = IV.find(wafer='wafer_name')
 dataframes = []
 for iv in ivs:
-    dataframes.append(iv.to_pandas_frame)
+    dataframes.append(iv.to_pandas_frame())
 for dataframe in dataframes:
     dataframe.plot(x="voltage", y="current")
 plt.show()
 
 # 2. Plotting all IV experiments from the same die onto the same graph
 
-ivs = IV.find(query={'die':'die_name'})
+ivs = IV.find(die='die_name')
 dataframes = []
 for iv in ivs:
-    dataframes.append(iv.to_pandas_frame)
+    dataframes.append(iv.to_pandas_frame())
 for dataframe in dataframes:
     dataframe.plot(x="voltage", y="current")
 plt.show()
 
 # 3. Plotting an IV experiment with an entire knowledge about the die
 
-iv = IV.find_one(query={'die' : 'die_name'})
-dataframe = iv.to_pandas_frame
+#Die you want to get IV from:
+die_name = 'blah'
+die = Die.find_one(die=die_name)
+#IV on die:
+iv = IV.find_one(die='die_name')
+#Plotting the IV experiment:
+dataframe = iv.to_pandas_frame()
 dataframe.plot(x="voltage", y="current")
 plt.show()
+#Print die information:
+print(die)
+#Print experiment information:
+print(dataframe)
+#Print wafer that die is on:
+wafer = die.get_wafer()
+print(wafer)
+#Print fabrun that wafer is on:
+fabrun = wafer.get_fabrun()
+print(fabrun)
+
+
 
